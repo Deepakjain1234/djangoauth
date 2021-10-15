9c99c5d3c6174f7458b326d4f8198e83d957c121
deepak <deeepakjain132@gmail.com>
2021-10-15 09:42:59 +0530

    final push

--- authdjango/settings.py
+++ authdjango/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uc8-tj^zd0=hi*zq-g@8vqmka3@cjj@e4i)pcyhjzp3ur6))n!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
